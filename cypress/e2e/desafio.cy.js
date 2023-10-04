ff50242e7310f03de48b3b86df4b33ef9a2281ed
Ricardo Oliveira <ricardooliveira.qa@gmail.com>
2023-10-03 21:58:23 -0300

    Subindo o desafio

---
+++ cypress/e2e/desafio.cy.js
@@ -0,0 +1,27 @@
+/// <reference types="cypress" />
+
+describe('Cadastro de usuário', ()=>{
+    it('Validar campo nome vazio', () => {
+        
+    });
+
+    it('Validar campo e-mail vazio', () => {
+        
+    });
+
+    it('Validar campo e-mail inválido', () => {
+        
+    });
+
+    it('Validar campo senha vazio', () => {
+        
+    });
+
+    it('Validar campo senha inválido', () => {
+        
+    });
+
+    it('Cadastro realizado com sucesso', () => {
+        
+    });
+})
