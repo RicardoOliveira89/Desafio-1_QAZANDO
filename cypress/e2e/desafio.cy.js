180cd255251d2d14aed13cb87587b0c1df08b878
Ricardo Oliveira <ricardooliveira.qa@gmail.com>
2023-10-04 21:02:24 -0300

    Subindo validação do campo e-mail vazio

--- cypress/e2e/desafio.cy.js
+++ cypress/e2e/desafio.cy.js
@@ -1,7 +1,7 @@
 /// <reference types="cypress" />
 
 describe('Cadastro de usuário', ()=>{
-    it.only('Validar campo nome vazio', () => {
+    it('Validar campo nome vazio', () => {
         cy.visit('/')
         cy.get('#top_header  li:nth-child(2) > a')
             .click()
@@ -11,8 +11,16 @@ describe('Cadastro de usuário', ()=>{
             .should('have.text', 'O campo nome deve ser prenchido')
     });
 
-    it('Validar campo e-mail vazio', () => {
-        
+    it.only('Validar campo e-mail vazio', () => {
+        cy.visit('/')
+        cy.get('#top_header  li:nth-child(2) > a')
+            .click()
+        cy.get('#user')
+            .type('Ricardo Oliveira')
+        cy.get('#btnRegister')
+            .click()
+        cy.get('#errorMessageFirstName')
+            .should('have.text', 'O campo e-mail deve ser prenchido corretamente')
     });
 
     it('Validar campo e-mail inválido', () => {
